d66dd4fe7438c62778221f053fb800c455177bc9
PascoPrzyborowski <unity01@gmx.net>
2022-08-24 14:06:17 +0200

    Ex10

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # String Basics 1
 
 ## Python Texts
---
+++ exercise10.py
@@ -0,0 +1,44 @@
+
+
+
+#Task1
+
+# city = "London"
+# print(city)
+
+#Task2
+
+# city = "berlin"
+# population = 3645000
+# print(city.capitalize() +':' + str(population))
+
+
+
+#Task3
+
+# city = "london"
+# population = "9000000"
+# print("City: " +city.capitalize() + " " +"("+str(city.isalpha())+")" +"\nPopulation: "+ population)
+
+#Task4
+
+# text = "Berlin is surrounded by the State of Brandenburg and Contiguous with Potsdam, Brandenburg's capital "
+# print("capital: " + str(text.index("capital")))
+
+#Task5
+
+# text = """Berlin straddles the banks of the Spree, which flows
+#  into the Havel (a tributary of the Elbe) in the 
+#  western borough of Spandau."""
+# list = text.split()
+# print(list)
+
+
+#Task6
+
+
+# text ="""Berlin is surrounded by the State of Brandenburg 
+# and  contiguous with Potsdam, Brandenburgs's capital"""
+# print(text)
+# print()
+# print(text.replace("capital","capital of Germany"))
